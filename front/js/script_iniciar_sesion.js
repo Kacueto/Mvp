bf94015ca5d81fbcf81a45c2df4f163241e3418a
Kacueto <75464308+Kacueto@users.noreply.github.com>
2023-11-26 23:18:11 -0500

    cambie el fetch

--- front/js/script_iniciar_sesion.js
+++ front/js/script_iniciar_sesion.js
@@ -43,7 +43,7 @@ function pedirDatos(usuario, contrasena){
             console.log("Datos a enviar al backend:", datosJSON);
     
             // Envía los datos al backend
-            fetch('web/verificar_login', {
+            fetch('http://localhost:5000/verificar_login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
